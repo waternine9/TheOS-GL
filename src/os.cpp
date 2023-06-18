b3e9c699fcc6f24f2d7b3a7dd0736aded5531e95
Andrew <andrew.vv.ivanchenko@gmail.com>
2023-06-18 05:24:14 -0400

    Fix VESA VBE

--- src/os.cpp
+++ src/os.cpp
@@ -5,4 +5,8 @@ extern "C" void
 kmain()
 {
     kmemset((uint8_t*)0x1000000 + 0x7C00, 0, 70000);
+    
+    ((uint8_t*)VbeModeInfo.framebuffer)[0] = 0xFF;
+    ((uint8_t*)VbeModeInfo.framebuffer)[1] = 0xFF;
+    ((uint8_t*)VbeModeInfo.framebuffer)[2] = 0xFF;
 }
